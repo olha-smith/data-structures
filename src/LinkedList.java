46430f02855deed2a6de82e30d80087086fc92fc
Olga Kovalenko <olha.smitholha@gmail.com>
2022-08-15 21:46:26 +0300

    Add LinkedList

---
+++ src/LinkedList.java
@@ -0,0 +1,32 @@
+public class LinkedList<T> implements List<T> {
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean add(T e) {
+        return false;
+    }
+
+    @Override
+    public T remove(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public T get(int index) throws IndexOutOfBoundsException {
+        return null;
+    }
+
+    @Override
+    public T set(int index, T element) throws IndexOutOfBoundsException {
+        return null;
+    }
+}
