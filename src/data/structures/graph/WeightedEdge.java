1eded42737d1140138b909f9a4f6d8269c2858cc
Olga Kovalenko <olha.smitholha@gmail.com>
2022-10-06 14:14:26 +0300

    Extend implementation of WeightedEdge
    Add constructor

--- src/data/structures/graph/WeightedEdge.java
+++ src/data/structures/graph/WeightedEdge.java
@@ -4,4 +4,10 @@ public class WeightedEdge<T> {
     Vertex<T> from;
     Vertex<T> to;
     Double weight;
+
+    public WeightedEdge(Vertex<T> from, Vertex<T> to, Double weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
 }
