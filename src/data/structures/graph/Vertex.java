4048b35e56e1d89ecd3741682793968a0c499483
Olga Kovalenko <olha.smitholha@gmail.com>
2022-10-10 12:28:34 +0300

    Add getter for Vertex.data property

--- src/data/structures/graph/Vertex.java
+++ src/data/structures/graph/Vertex.java
@@ -4,6 +4,10 @@ public class Vertex<T> {
     int index;
     T data;
 
+    public T getData() {
+        return data;
+    }
+
     Vertex (int index, T data) {
         this.index = index;
         this.data = data;
